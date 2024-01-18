8cadd68da773dd9fa1ad4734592f736a9d949705
Sean Scriven <tyscrivense@gmail.com>
2024-01-18 16:37:27 +0000

    Initial commit for clare

---
+++ index.php
@@ -0,0 +1,13 @@
+<!--!DOCTYPE html-->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Clare Scriven Music</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Stylesheets-->
+  </head>
+
+  <body>
+    Hello World
+  </body>
+</html>
