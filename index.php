4b18046da6d391cac2c365ec04d36758a028b394
Sean Scriven <tyscrivense@gmail.com>
2024-06-25 12:59:31 +0100

    Webpage temporay

--- index.php
+++ index.php
@@ -3,86 +3,219 @@
     <head>
         <meta charset="utf-8">
         <title>Clare Scriven Music</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Stylesheets-->
         <link type="text/css" rel="stylesheet" media="all" href="webroot/css/bootstrap.min.css">
         <link href="node_modules/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet'>
+        <link href='https://fonts.googleapis.com/css?family=Alfa Slab One' rel='stylesheet'>
         <link type="text/css" rel="stylesheet" media="all" href="webroot/css/clare-music.css">
 
         <script src="webroot/js/clare-scriven.js"></script>
     </head>
 
     <body>
-        <div class="row header">
-            <div class="heading">Clare Scriven</div>
-            <div class="header-nav col-12">
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Bio</a></li>
-                    <li><a href="">Music</a></li>
-                    <li><a href="">Contact</a></li>
-                </ul>
-            </div>
-        </div>
-
         <div class="mobile-header">
             <div class="mobile-header-container">
-                <div class="menu-dropdown">
-                    <button type="button" class="btn list-button" onClick="showMobileNavList()">
-                        <i class="bi bi-list"></i>
+                <div class="mobile-heading-container">
+                    <h1 class="heading">CLARE SCRIVEN</h1>
+                </div>
+                <div class="mobile-menu-dropdown">
+                    <button id="mobile-nav-button" type="button" class="btn mobile-list-button" onClick="showMobileNavList()">
+                        <i class="bi bi-caret-down-fill mobile-list-icon"></i>
                     </button>
                 </div>
-                <div class="heading-container">
-                    <h1 class="heading">Clare Scriven</h1>
-                </div>
             </div>
-            <div id="mobile-nav" class="header-nav">
+            <div id="mobile-nav" class="header-nav hide-mobile-list">
                 <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Bio</a></li>
-                    <li><a href="">Music</a></li>
-                    <li><a href="">Contact</a></li>
+                    <li><a href="#section-home" onClick="closeMobileNavList()">Home</a></li>
+                    <li><a href="#section-bio" onClick="closeMobileNavList()">Bio</a></li>
+                    <li><a href="#section-music" onClick="closeMobileNavList()">Music</a></li>
+                    <li><a href="#section-contact" onClick="closeMobileNavList()">Contact</a></li>
                 </ul>
             </div>
         </div>
 
-
-        <div class="row">
-            <p class="col-12">
-                Clare Scriven is a pop singer, songwriter, and producer from County Cork, Ireland.
-                Clare’s music is a fusion of synth-pop, nu-disco,
-                and future bass that intertwines profound lyricism and evocative imagery.
-            </p>
+        <div class="main-header">
+            <div class="main-heading-container">
+                <h1 class="heading">CLARE SCRIVEN</h1>
+            </div>
+            <div class="main-nav">
+                <ul>
+                    <li><a href="#section-home">Home</a></li>
+                    <li><a href="#section-bio">Bio</a></li>
+                    <li><a href="#section-music">Music</a></li>
+                    <li><a href="#section-contact">Contact</a></li>
+                </ul>
+            </div>
         </div>
 
-        <div class="row">
-            <h1>Bio</h1>
+        <div class="row main-section">
+            <div id="section-home" class="section">
+                <div class="inside-section">
+                    <div class="home-container">
+                        <div class="content-container">
+                            <div class="content-popup">
+                                <div class="content-header">
+                                    <div class="close-button">x</div>
+                                </div>
+                                <div class="content-window">
+                                    <p class="home-content col-12">
+                                        Clare Scriven is a pop singer, songwriter, and producer from County Cork, Ireland.
+                                        Clare’s music is a fusion of synth-pop, nu-disco,
+                                        and future bass that intertwines profound lyricism and evocative imagery.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="content-container image-container">
+                            <img src="webroot/img/guitars.jpg" alt="guitars">
+                        </div>
+                    </div>
 
-            <p class="col-12">
-                Clare Scriven is a singer, songwriter, and producer based in Cork City.
-                She started performing at age ten in The Everyman Palace Theatre and The Cork Opera House with the stage school Studio Wolfe.
-                Clare attended The Academy of Popular Music at age sixteen where she performed gigs in venues such as Cyprus Avenue and The Oliver Plunkett.
-                She attended the MTU Cork School of Music where she completed a BA in Popular Music with Voice (Hons),
-                and an MA in Music Performance. Her current musical style is largely influenced by pop-synth, nu-disco,
-                and future bass, however she has studied many different styles of singing such as musical theatre,
-                blues, rock, funk, and country.
-            </p>
+                    <div id="demo-audio" class="audio-bubble">
+                        <!-- here is where the demo audio goes. Created from in the js file -->
+                    </div>
+                </div>
+            </div>
 
-            <p class="col-12">
-                Clare has performed her original music and arrangements in the MTU Cork School of Music as part of her MA Recitals.
-                She is currently on her journey to releasing a debut single under the artist project name “Jainey”.
-                With an EP scheduled to follow, this music project will be a reflection of Clare’s experience
-                in society and the current climate of the world.
-            </p>
+            <div id="section-bio" class="section bio-section">
+                <div class="inside-section">
+                    <h1>Bio</h1>
+                    <div class="bio-container">
+                        <div class="bio-child-container bio-child-first-container">
+                            <div class="content-popup">
+                                <div class="content-header">
+                                    <div class="close-button">x</div>
+                                </div>
+                                <div class="content-window">
+                                    <p class="bio-content col-12">
+                                        Clare Scriven is a singer, songwriter, and producer based in Cork City.
+                                        She started performing at age ten in The Everyman Palace Theatre and The Cork Opera House with the stage school Studio Wolfe.
+                                        Clare attended The Academy of Popular Music at age sixteen where she performed gigs in venues such as Cyprus Avenue and The Oliver Plunkett.
+                                        She attended the MTU Cork School of Music where she completed a BA in Popular Music with Voice (Hons),
+                                        and an MA in Music Performance. Her current musical style is largely influenced by pop-synth, nu-disco,
+                                        and future bass, however she has studied many different styles of singing such as musical theatre,
+                                        blues, rock, funk, and country.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="bio-child-container bio-image-roll-container">
+                            <div class="bio-image-box bio-image-box-taller">
+                                <img src="webroot/img/angel.jpg" alt="angel">
+                            </div>
+                            <div class="bio-camera-roll">
+                                <img src="webroot/img/apartments.jpg" alt="apartments">
+                                <img src="webroot/img/window.jpg" alt="window">
+                                <img src="webroot/img/bears.jpg" alt="bears">
+                            </div>
+                        </div>
+                        <div class="bio-child-container bio-image-container">
+                            <div class="bio-image-box">
+                                <img src="webroot/img/excel.jpg" alt="excel">
+                            </div>
+                            <div class="bio-image-box hovered-image-box">
+                                <img src="webroot/img/cycling.jpg" alt="cycling">
+                            </div>
+                        </div>
+                        <div class="bio-child-container">
+                            <div class="content-popup">
+                                <div class="content-header">
+                                    <div class="close-button">x</div>
+                                </div>
+                                <div class="content-window">
+                                    <p class="bio-content col-12">
+                                        Clare has performed her original music and arrangements in the MTU Cork School of Music as part of her MA Recitals.
+                                        She is currently on her journey to releasing a debut single under the artist project name “Jainey”.
+                                        With an EP scheduled to follow, this music project will be a reflection of Clare’s experience
+                                        in society and the current climate of the world.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="section-music" class="section music-section">
+                <div class="inside-section">
+                    <h1>Music</h1>
+
+                    <div id="audio-list" class="audio-section audio-bubble">
+                        <!-- here is where the song list goes. Created from in the js file -->
+                    </div>
+
+                    <div class="video-section">
+                        <div class="video-container">
+                            <iframe
+                                class="video-frame"
+                                src="https://www.youtube.com/embed/Y8EPs0D7siE?si=FbHFE-fTq5de1RzJ"
+                                title="YouTube video player"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowfullscreen
+                            >
+                            </iframe>
+                        </div>
+                        <div class="video-container">
+                            <iframe
+                                class="video-frame"
+                                src="https://www.youtube.com/embed/JKrDdsgXuso?si=8fkD7DcqUavwyIO7"
+                                title="YouTube video player"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowfullscreen
+                                >
+                                </iframe>
+                        </div>
+                        <div class="video-container">
+                            <iframe
+                                class="video-frame"
+                                src="https://www.youtube.com/embed/uMCv9djEm1k?si=xvTex9C8a_oiMCDO"
+                                title="YouTube video player"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowfullscreen
+                                >
+                                </iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div class="row">
-            <h1>Music</h1>
+        <div class="row footer" id="section-contact">
+            <h1 class="contact-header">Contact</h1>
+
+            <div class="contact-nav">
+                <ul>
+                    <li>
+                        <a href="mailto:clarescrivenmusic@gmail.com">
+                            <i class="bi bi-envelope"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/clare_sinead?igsh=ODA1NTc5OTg5Nw%3D%3D&utm_source=qr">
+                            <i class="bi bi-instagram"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.tiktok.com/@clur_s?_t=8il7xa2eDYG&_r=1">
+                            <i class="bi bi-tiktok"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://youtube.com/@clarescriven2815?si=9O3qXrU50jSYPJSL">
+                            <i class="bi bi-youtube"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
 
-
-        <div class="row footer">
-            <h1>Contact</h1>
-        </div>
     </body>
+    <script>
+        displayAllAudios()
+    </script>
 </html>
--- webroot/js/clare-scriven.js
+++ webroot/js/clare-scriven.js
@@ -1,8 +1,157 @@
+const demoAudio = {
+    id: 1,
+    name: 'The Weatherman (Chorus)',
+    totalDuration: 28,
+    currentDuration: 0,
+    location: 'webroot/music/tw-chorus-clip.wav'
+}
+
+const audioFileList = [{
+    id: 2,
+    name: 'Coming Down',
+    totalDuration: 172,
+    currentDuration: 0,
+    location: 'webroot/music/coming-down.wav'
+},
+{
+    id: 3,
+    name: 'The Weatherman',
+    totalDuration: 190,
+    currentDuration: 0,
+    location: 'webroot/music/the-weatherman.wav'
+}];
+
+document.addEventListener('click', function(event) {
+    const hiddenDiv = document.getElementById('mobile-nav');
+    const menuButton = document.getElementById('mobile-nav-button');
+    if (!menuButton.contains(event.target) && !hiddenDiv.contains(event.target) && hiddenDiv.classList.contains('show-mobile-list')) {
+        hiddenDiv.classList.add('hide-mobile-list');
+        hiddenDiv.classList.remove('show-mobile-list');
+    }
+})
+
 function showMobileNavList () {
     const hiddenDiv = document.getElementById('mobile-nav');
-    if (hiddenDiv.style.display === 'none' || hiddenDiv.style.display === '') {
-        hiddenDiv.style.display = 'block';
+    if (hiddenDiv.classList.contains('hide-mobile-list')) {
+        hiddenDiv.classList.add('show-mobile-list');
+        hiddenDiv.classList.remove('hide-mobile-list');
     } else {
-        hiddenDiv.style.display = 'none';
+        hiddenDiv.classList.add('hide-mobile-list');
+        hiddenDiv.classList.remove('show-mobile-list');
     }
+}
+
+function closeMobileNavList () {
+    const hiddenDiv = document.getElementById('mobile-nav');
+    hiddenDiv.classList.add('hide-mobile-list');
+    hiddenDiv.classList.remove('show-mobile-list');
+}
+
+function displayAllAudios () {
+    displayAudioList();
+    displayDemoAudio();
+}
+
+function displayDemoAudio () {
+    const audioList = document.getElementById('demo-audio');
+    audioList.innerHTML = ''; // Clear previous content
+    const audioGroup = createAudioContent(demoAudio);
+    audioList.appendChild(audioGroup);
+}
+
+function displayAudioList () {
+    const audioList = document.getElementById('audio-list');
+    audioList.innerHTML = ''; // Clear previous content
+    audioFileList.forEach((audio) => {
+        const audioGroup = createAudioContent(audio);
+        audioList.appendChild(audioGroup);
+    })
+}
+
+function createAudioContent (audio) {
+    // the audio item to be added to the list in the html page for each audio
+    const audioGroup = document.createElement('div');
+    audioGroup.classList.add('audio-group');
+    audioGroup.setAttribute('id', `audio-group-${audio.id}`);
+    // the div for the button for the audio
+    const audioButton = document.createElement('div');
+    audioButton.classList.add('audio-buttons');
+    // the button to be displayed for the audio (starts off as play button)
+    const buttonItem = document.createElement('button');
+    buttonItem.type = 'button';
+    buttonItem.classList.add('btn', 'list-button', 'play-button');
+    buttonItem.setAttribute('id', `audio-button-${audio.id}`);
+    const buttonContent = document.createElement('i');
+    buttonContent.classList.add('bi', 'bi-play-fill');
+    buttonItem.appendChild(buttonContent);
+    buttonItem.addEventListener('click', () => clickPlayButton(audio.id));
+    // add the play button to the audio div
+    audioButton.appendChild(buttonItem);
+    // add the all the audio div with play button included to the group div for the item
+    audioGroup.appendChild(audioButton);
+    // now add name to audio group div
+    const audioName = document.createElement('div');
+    audioName.classList.add('audio-name');
+    const nameSpan = document.createElement('span');
+    nameSpan.textContent = audio.name;
+    // add the audio tag along with the name to be able to display the audio file
+    const audioContent = document.createElement('audio');
+    audioContent.src = audio.location;
+    audioContent.type = 'audio/wav';
+    audioContent.setAttribute('id', `audio-file-${audio.id}`);
+    audioContent.addEventListener('timeupdate', () => updateDuration(audio));
+    audioContent.addEventListener('ended', () => audioEnded(audio.id));
+    audioName.appendChild(nameSpan);
+    audioName.appendChild(audioContent);
+    audioGroup.appendChild(audioName);
+    // now add duration to audio with the current time played and the total duration
+    const durationContent = document.createElement('div');
+    durationContent.classList.add('audio-duration');
+    const durationSpan = document.createElement('span');
+    durationSpan.setAttribute('id', `audio-duration-${audio.id}`)
+    durationSpan.textContent = `${formatTime(audio.currentDuration)}/${formatTime(audio.totalDuration)}`;
+    durationContent.appendChild(durationSpan);
+    audioGroup.appendChild(durationContent);
+    return audioGroup;
+}
+
+function clickPlayButton (audioId) {
+    const clickedButton = document.getElementById(`audio-button-${audioId}`);
+    const buttonIcon = clickedButton.querySelector('i');
+    const audioPlayer = document.getElementById(`audio-file-${audioId}`);
+    if (buttonIcon.classList.contains('bi-play-fill')) {
+        showPauseButton(buttonIcon);
+        audioPlayer.play();
+    } else {
+        showPlayButton(buttonIcon);
+        audioPlayer.pause();
+    }
+}
+
+function updateDuration (audio) {
+    const durationSpan = document.getElementById(`audio-duration-${audio.id}`);
+    const audioPlayer = document.getElementById(`audio-file-${audio.id}`);
+    durationSpan.textContent = `${formatTime(audioPlayer.currentTime)}/${formatTime(audio.totalDuration)}`
+}
+
+function audioEnded (audioId) {
+    const clickedButton = document.getElementById(`audio-button-${audioId}`);
+    const buttonIcon = clickedButton.querySelector('i');
+    showPlayButton(buttonIcon);
+}
+
+function showPlayButton (buttonIcon) {
+    buttonIcon.classList.remove('bi-pause-fill');
+    buttonIcon.classList.add('bi-play-fill');
+}
+
+function showPauseButton (buttonIcon) {
+    buttonIcon.classList.remove('bi-play-fill');
+    buttonIcon.classList.add('bi-pause-fill');
+}
+
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
 }
